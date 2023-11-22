1e4c7b4a50745b1835d9543843d58e863bfed723
Aurélien Georget <aurelsicoko@gmail.com>
2023-11-22 18:41:42 +0100

    Add default values for env variables

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'xxxxxxxxx'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'xxxxxxxxx'),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'xxxxxxxxx'),
     },
   },
   flags: {
